2e9ab96228494408844bb32636c49ae846ab45bb
Daniel Goehring <daniel.goehring@gmail.com>
2026-07-23 12:04:29 +0200

    Update mandelbrot.py
    debug, more comments

--- python/mandelbrot.py
+++ python/mandelbrot.py
@@ -6,10 +6,10 @@ from PIL import Image
 def mandelbrot():
     #print("Hallo")
 
-#    w, h = 3840, 2048
-    w, h = 1024, 512
+    w, h = 1024, 512  #determines the resolution of the output image
 
-    maxiter = 100
+    maxiter = 100     #number of max iterations, higher values lead to more precise and detailed results
+    threshold = 50    #to determine if the series does not converge, higher values lead to more nuanced contours
 
     data = np.zeros((h, w, 3), dtype=np.uint8)
     ##data[0:256, 0:256] = [255, 0, 0]  # red patch in upper left
@@ -22,14 +22,14 @@ def mandelbrot():
             #print(c)
             for it in range(maxiter):
                 z = z*z + c
-                if z.__abs__() > 100:
+                if z.__abs__() > threshold:  
                     data[y,x] = [it * 255/maxiter,0,0]
                     #the most iterations lead to the brightest colors, large numbers darker that don't converge, exceed the bound quicker and are painted darker.
                     #print(it)
                     break
 
     img = Image.fromarray(data, 'RGB')
-    #img.save('foo.png')
+    #img.save('mandelbrot.png')
     img.show()
 
     pass
